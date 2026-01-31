898b6b0e1a43a45618075ef409efcd014a96ea63
Shivam Gupta <shivamgupta191092005@gmail.com>
2026-01-31 20:43:44 +0530

    Refactor Langgraph notebook: update execution counts, modify tool invocation, and adjust initial state for multiplication task

---
+++ MCP_Langgraph/client.py
@@ -0,0 +1,23 @@
+from langgraph.graph import StateGraph,START,END
+from langgraph.prebuilt import tools_condition,ToolNode
+from langchain_ollama import ChatOllama
+from langchain_core.tools import tool
+from langchain_core.messages import HumanMessage,SystemMessage
+from langgraph.graph.message import add_messages
+from langchain_community.tools import DuckDuckGoSearchRun
+
+from pydantic import BaseModel
+from typing import List,Annotated
+from asyncio
+
+
+model = ChatOllama(model="ministral-3:3b")
+
+# search tool 
+search_tool = DuckDuckGoSearchRun(region = 'us-en')
+
+# 
+# result = search_tool.invoke("today news in ai?")
+
+# print("Search Result:", result)
+
