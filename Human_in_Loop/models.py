c49fcae1c14db834155992ea8299a74c0579268f
Shivam Gupta <shivamgupta191092005@gmail.com>
2026-02-04 21:40:05 +0530

    minor ignorance in error

--- Human_in_Loop/models.py
+++ Human_in_Loop/models.py
@@ -13,5 +13,5 @@ eval_model = ChatOllama(model="qwen3:1.7b") # for evaluation
 structured_parser_model = eval_model.with_structured_output(EvaluationSchema) 
 
 # 2. Generation Model
-generation_model = ChatOllama(model="ministral-3:3b") # for generation
+generation_model = ChatOllama(model="ministral-3:3b") # type:ignore for generation
 # generation_model = ChatOllama(model="deepseek-r1:1.5b") # for generation
